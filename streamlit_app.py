e3310a992ebc8c255e5e66c15fb64bb0a7e14e90
Phil Perna <philjp73@gmail.com>
2024-06-15 05:56:50 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,8 @@ cnx = st.connection("snowflake")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response).json()
+#st.text(fruityvice_response).json()
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
